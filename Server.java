e926480600efe264871c31e3938dd92d391582ed
DipaHub3 <diepasotti@gmail.com>
2026-03-06 16:21:03 +0000

    first commit

---
+++ Server.java
@@ -0,0 +1,9 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class Server{
+    public static void main(String [] args){
+        try(Socket socket = new Socket("localhost,8080"))
+    }
+}
