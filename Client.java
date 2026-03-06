7458471ed1854dad1646c06e66d059f284eaa82a
DipaHub3 <diepasotti@gmail.com>
2026-03-06 16:38:08 +0000

    second commit

---
+++ Client.java
@@ -0,0 +1,22 @@
+import java.net.*;
+import java.util.Scanner;
+import java.io.*;
+
+public class Client{
+    public static void main(String[] args) {
+        try(Socket socket = new Socket("localhost",8080)){
+        
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            String input = "";
+            Scanner scanner = new Scanner(System.in);
+
+            while(!input.equals("addio")){
+                String riga = scanner.nextLine();
+                out.println(riga);    
+            }
+        }catch(IOException e){}
+
+    }
+}
--- Server.java
+++ Server.java
@@ -4,6 +4,12 @@ import java.util.*;
 
 public class Server{
     public static void main(String [] args){
-        try(Socket socket = new Socket("localhost,8080"))
+        try(Socket socket = new Socket("localhost",8080)){
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            
+            if(in.readLine()!=null) System.out.println("ciao");
+            
+        }catch(IOException e){}
     }
 }
