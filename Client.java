1db242a4aa56fc0b48b553fa66423859e917d08b
Diego Pasotti <dieapasotti@gmail.com>
2026-03-10 12:34:28 +0100

    Sistemati Client e Server per comunicazione

--- Client.java
+++ Client.java
@@ -3,22 +3,34 @@ import java.util.Scanner;
 import java.io.*;
 
 public class Client{
+    static PrintWriter out;
+    static BufferedReader in;
     public static void main(String[] args) {
         try(Socket socket = new Socket("localhost",8080)){
         
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(), true); //lettura e scrittura sulla socket 
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
             String input = "";
             Scanner scanner = new Scanner(System.in);
-
-            while(!input.equals("addio")){
-                String riga = scanner.nextLine();
-                out.println(riga);    
+            
+            while(true){ //ciclo infinito per inviare i messaggi al server, lui risponde con una risposta automatica
+                input = scanner.nextLine();
+                if(input.equals("exit")){
+                    break;
+                }
+                out.println(input);
+                System.out.println("In attesa di una risposta dal server...");
+                String riga = in.readLine();  
+                System.out.println("Risposta dal server: " + riga);
             }
+        in.close();
+        out.close();
+        scanner.close();
         }catch(IOException e){
             e.printStackTrace();
         }
+        
 
     }
 }
--- Server.java
+++ Server.java
@@ -1,24 +1,36 @@
 import java.net.*;
 import java.io.*;
-import java.util.*;
+
 
 public class Server{
+    static PrintWriter out;
+    static BufferedReader in;
     public static void main(String [] args){
-        //while (true) {
+        try(ServerSocket serversocket = new ServerSocket(8080)){ //creazione del server socket sulla porta 8080
+                
+            try(Socket client = serversocket.accept()){ //accettazione della connessione da parte del client, se c'è una connessione in attesa, altrimenti rimane in attesa
+                    
+            out = new PrintWriter(client.getOutputStream(), true); //lettura e scrittura sulla socket specifica del client che si è connesso
+            in = new BufferedReader(new InputStreamReader(client.getInputStream()));
             
-        try(ServerSocket serversocket = new ServerSocket(8080)){
-            try(Socket client = serversocket.accept()){
-            PrintWriter out = new PrintWriter(client.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                while (true) { //ciclo infinito per ricevere i messaggi dal client, lui risponde con una risposta automatica
+                    System.out.println("In attesa di un messaggio...");
+                    String msg = in.readLine();    
+                    if(!msg.equals("")){ 
+                        System.out.println("Messaggio ricevuto: " + msg);
+                        out.println("Ciao, benvenuto al server!");
+                    }
+                }
             
-            if(in.readLine()!=null) System.out.println("ciao");
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+        in.close();
+        out.close();
+                       
+        }catch(IOException e){}
             
-        }catch(IOException e){
-            e.printStackTrace();
-        }
-    }catch(IOException e){
+    }
+}        
+    
 
-    }
-    //}
-    }
-}
